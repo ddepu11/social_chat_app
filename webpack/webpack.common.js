882aae14b8bcd183c30ee898cc908fe09958960d
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-12 16:42:47 +0530

    chore: add babel support

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -11,6 +11,21 @@ module.exports = {
     }),
   ],
 
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "../build"),
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,4 +1,8 @@
 module.exports = {
   mode: "production",
   devtool: "source-map",
+
+  output: {
+    clean: true,
+  },
 };
