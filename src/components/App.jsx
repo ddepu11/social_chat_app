7e56ce78f2dc64d4a38dbcf264614cb1754e67b2
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-12 16:59:03 +0530

    chore: add styled-components and creat index and app components

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const App = () => {
+  console.log("App");
+
+  return (
+    <Wrapper>
+      <h2>Hello Social Chat app</h2>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.main``;
+
+export default App;
