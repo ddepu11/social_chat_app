549633caa77a41d1c52c8d847195da1f0fe2ec4a
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-12 17:42:25 +0530

    chore: add css-loader, style-loader

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -23,6 +23,11 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 
