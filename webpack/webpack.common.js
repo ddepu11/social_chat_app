fff5fc90510ba05fe38d7daa22544ee5cb856cf9
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-12 16:26:23 +0530

    chore: add html-webpack-plugin for html template

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,7 +1,15 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "../src/index.js"),
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.resolve(__dirname, "../src/index.html"),
+    }),
+  ],
 
   output: {
     filename: "bundle.js",
