633e1fc0afd50b218f0c83ca4535ab6951a9a26f
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-13 15:35:57 +0530

    chore: configure firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDn23ndp7XyfPp2_dI_Ob3uUf7ZG6ZoBqs',
+  authDomain: 'socialchatapp-b781f.firebaseapp.com',
+  projectId: 'socialchatapp-b781f',
+  storageBucket: 'socialchatapp-b781f.appspot.com',
+  messagingSenderId: '668891353518',
+  appId: '1:668891353518:web:7fefe98f936e23e58db83f',
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const authInstance = getAuth();
+const firestoreInstance = getFirestore();
+
+export { authInstance, firestoreInstance };
