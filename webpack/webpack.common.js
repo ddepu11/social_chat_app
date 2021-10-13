d43cb493a92b4dba7ea835a2e3f05fc5f8be3388
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-13 13:43:27 +0530

    chore: add  eslint and prettier support

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,15 +1,15 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  target: "web",
+  target: 'web',
 
-  entry: path.resolve(__dirname, "../src/index.jsx"),
+  entry: path.resolve(__dirname, '../src/index.jsx'),
 
   plugins: [
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.resolve(__dirname, "../src/index.html"),
+      filename: 'index.html',
+      template: path.resolve(__dirname, '../src/index.html'),
     }),
   ],
 
@@ -19,9 +19,9 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
           },
         },
       },
@@ -29,25 +29,25 @@ module.exports = {
       {
         test: /\.css$/i,
         exclude: /node_modules/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
 
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         exclude: /node_modules/,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
 
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "../build"),
-    assetModuleFilename: "images/[hash][ext][query]",
-    publicPath: "/",
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../build'),
+    assetModuleFilename: 'images/[hash][ext][query]',
+    publicPath: '/',
   },
 
   resolve: {
-    extensions: [".js", ".json", ".jsx"],
+    extensions: ['.js', '.json', '.jsx'],
   },
 };
