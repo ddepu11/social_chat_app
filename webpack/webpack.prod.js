79e93f3be0cb8a1bee872dc1a6effdb5619917eb
Deepanshu Tiwari <ddepu11@gmail.com>
2021-10-16 23:21:52 +0530

    fix: on refresh redirect to /

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,19 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
+
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../src/_redirects'),
+          to: path.resolve(__dirname, '../build'),
+        },
+      ],
+    }),
+  ],
 
   output: {
     clean: true,
